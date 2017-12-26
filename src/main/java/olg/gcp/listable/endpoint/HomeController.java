30c9907483579ce03234662c739acc841cee7c92
Olga <olga.penina@emc.com>
2017-12-25 23:51:00 -0500

    Configure CORS registry

--- src/main/java/olg/gcp/listable/endpoint/HomeController.java
+++ src/main/java/olg/gcp/listable/endpoint/HomeController.java
@@ -1,11 +1,9 @@
 package olg.gcp.listable.endpoint;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:8080")
 public class HomeController {
 
     @RequestMapping(value = "/")
--- src/main/java/olg/gcp/listable/repository/HouseRepository.java
+++ src/main/java/olg/gcp/listable/repository/HouseRepository.java
@@ -2,7 +2,9 @@ package olg.gcp.listable.repository;
 
 import olg.gcp.listable.model.House;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "houses", path = "houses")
 public interface HouseRepository  extends CrudRepository<House, Long> {
 
 }
