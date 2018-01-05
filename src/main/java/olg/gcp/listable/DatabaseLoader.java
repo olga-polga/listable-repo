b1b37d3d1799b7586df737bb7ce5270f1f15ac16
owner <olga.penina@emc.com>
2018-01-05 18:11:24 -0500

    Cloud SQL configuration

--- src/main/java/olg/gcp/listable/DatabaseLoader.java
+++ src/main/java/olg/gcp/listable/DatabaseLoader.java
@@ -24,16 +24,22 @@ public class DatabaseLoader implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         House saved = null;
         saved = this.houseRepo.save(new House("5 Privet Dr." ));
-
+        saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-11111")));
+        saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-10101")));
+        this.houseRepo.save(saved);
         saved = this.houseRepo.save(new House("720 Paper St." ));
         saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-55555")));
         saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-15151")));
         saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-50105")));
         this.houseRepo.save(saved);
         saved = this.houseRepo.save(new House("Apt. 56B, Whitehaven Mansions" ));
+        saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-33333")));
+        saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-33030")));
+        this.houseRepo.save(saved);
         saved = this.houseRepo.save(new House("124 Conch Street, Bikini Bottom" ));
         saved = this.houseRepo.save(new House("Mulholland Drive, Los Angeles, CA" ));
         saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-99999")));
         saved.getPictures().add(this.imageRepo.save(new ImageUrl("`https://images.unsplash.com/photo-90909")));
+        this.houseRepo.save(saved);
     }
 }
